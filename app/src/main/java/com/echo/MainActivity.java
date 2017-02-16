87b3864bd686c73f36bb39882f7286da8cff6254
s.lain <vvvb78@gmail.com>
2017-02-16 21:42:55 +0900

    2017-02-16 2
    배경화면 Glide resizing 필요

--- app/src/main/java/com/echo/MainActivity.java
+++ app/src/main/java/com/echo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.echo;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.floatingActionButton)FloatingActionButton fab;
     @BindView(R.id.recondingLayout)CardView recordingLayout;
     @BindView(R.id.toolbar)Toolbar toolbar;
+    @BindView(R.id.activity_main)CoordinatorLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
         fab.setOnClickListener(this);
         recordingLayout.setOnClickListener(this);
+        background.setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(recordingLayout.getVisibility() == View.VISIBLE){
+            //TODO if recordind...
+            recordingLayout.setVisibility(View.GONE);
+        } else{
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -34,20 +49,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.floatingActionButton:
                 //fab을 클릭하면 fab는 오른쪽으로 사라지고 layout은 위로 올라온다.
                 Animation slideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);//for recordLayout
-                Animation slideLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_left);//for fab
+                Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);//for fab
 
-                view.startAnimation(slideLeft);
+                view.startAnimation(fadeOut);
                 view.setVisibility(View.GONE);
 
                 recordingLayout.startAnimation(slideUp);
                 recordingLayout.setVisibility(View.VISIBLE);
                 break;
-            case R.id.recondingLayout:
+            case R.id.activity_main:
                 //layout을 터치하면 fab는 왼쪽에서 나타나고 layout은 아래로 내려간다.
                 Animation slideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);//for recordLayout
-                Animation slideRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_right);//for fab
+                Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);//for fab
 
-                fab.startAnimation(slideRight);
+                fab.startAnimation(fadeIn);
                 fab.setVisibility(View.VISIBLE);
 
                 view.startAnimation(slideDown);
